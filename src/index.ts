5c94a3cba3c4e2626ba4db24cec6ab37f4c10d4b
andre.pinheiro <andre.pinheiro@digitro.com>
2021-11-06 09:10:04 -0300

    chore: add express and nodemon

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+import routers from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routers);
+
+app.listen(3000, () => console.log('Server running at http://localhos:3000'));
